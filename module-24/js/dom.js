b8bf795a1d273bc7418fdae0677e7ddb78c3cad7
SohelShahid117 <sohelshahid09@gmail.com>
2024-02-10 09:59:00 +0600

    complete the vdo 24-5

--- module-24/js/dom.js
+++ module-24/js/dom.js
@@ -1,5 +1,6 @@
 // vdo-24-1: What Is Dom, Connect Your JS File With Html File
 // 24-2 Traversing Dom - GetElementsByTagName
+
 console.log(`hello from JS-->DOM:Document Object Model`);
 console.log(document);
 console.log(document.body);
@@ -20,4 +21,72 @@ for(const h1 of allHeading){
     console.log(h1.innerText);
 }
 
+
+
 //24-3 Traversing Dom - GetElementByClass VS GetElementById
+
+console.log(`----------------------------`)
+const fruitsTitle = document.getElementById('fruits-title');
+console.log(fruitsTitle);
+fruitsTitle.innerText = `Fruits chande by JS`;
+
+console.log(document.getElementById("places-title"));
+console.log(document.getElementById("places-title").innerText);
+console.log(document.getElementById("places-title").innerText = "visiting place ");
+const placesss = document.getElementsByClassName('impotant-places');
+console.log(placesss);
+for(place of placesss){
+    // console.log(place);
+    console.log(place.innerHTML);
+    console.log(place.innerText);
+}
+
+
+
+// 24-4 Traversing Dom - GetElementByQuerySelector, QuerySelectorAll
+console.log(`----------------------------`);
+const someLi = document.querySelectorAll('.fruits-container li');
+console.log(someLi);
+for(li of someLi){
+    console.log(li);
+    console.log(li.innerText);
+}
+// only for 1st element
+const firstLi = document.querySelector('.fruits-container li');
+console.log(firstLi)
+console.log(firstLi.innerHTML)
+
+
+
+//24-5 Dynamic Style, GetAttribute, SetAttribute, InnerText, InnerHTML
+console.log(`----------------------------`);
+/*
+document.getElementById('fruits-title').style;
+document.getElementById('fruits-title').style.color=`blue`
+document.getElementById('fruits-title').style.backgroundColor=`gray`
+document.getElementById('fruits-title').style.textAlign = `center`
+*/
+/*
+const title = document.getElementById('fruits-title')
+console.log(title.getAttribute('id'));
+console.log(title.getAttribute('class'));
+console.log(title.classList);
+
+console.log(title.classList.remove('bgc-class'));
+title.classList.remove('bgc-class');
+console.log(title.classList);
+
+title.classList.add('purple-class');
+console.log(title.classList);
+title.setAttribute('title','set attribute now------>tooltip set by JS');
+
+document.getElementsByClassName('fruits-container')
+document.getElementsByClassName('fruits-container')[0]
+document.getElementsByClassName('fruits-container')[0].innerHTML
+document.getElementsByClassName('fruits-container')[0].innerText;
+document.getElementsByClassName('fruits-container')[0].innerHTML = '<h2>great ABC-DOM changing the content</h2>'
+*/
+
+
+
+//24-6 Styling Dom Properties, Add And Remove Css Class Using JS
