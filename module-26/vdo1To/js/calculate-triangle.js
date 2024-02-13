13806ab815bd4416f688644f06c6a1fa851b65fb
SohelShahid117 <sohelshahid09@gmail.com>
2024-02-13 23:25:27 +0600

    complete the vdo-26-5

--- module-26/vdo1To/js/calculate-triangle.js
+++ module-26/vdo1To/js/calculate-triangle.js
@@ -1,4 +1,27 @@
+// 26-1 Project Overview And Setup
+// 26-2 Layout Design And Card Design
+// 26-3 Get Triangle Base And Height Using Event And Function
+
+
 // console.log(`calculate triangle`)
 function calculateTriangle(){
-    console.log(`inside from the calculator function`)
+    // console.log(`inside from the calculator function`);
+    const triangleBaseInput = document.getElementById('triangle-base');
+    // console.log(triangleBaseInput)
+    const triangleBaseValue=triangleBaseInput.value;
+    // console.log(triangleBaseValue)
+    const base=parseFloat(triangleBaseValue);
+    // console.log(base)
+
+    const triangleHeightInput = document.getElementById('triangle-height');
+    // console.log(triangleBaseInput);
+    const triangleHeightValue=triangleHeightInput.value;
+    // console.log(triangleHeightValue);
+    const height=parseFloat(triangleHeightValue);
+    // console.log(height)
+
+    // console.log(base);
+    // console.log(height);
+    const area = 0.5*base*height;
+    document.getElementById('triangle-area').innerText=area;
 }
