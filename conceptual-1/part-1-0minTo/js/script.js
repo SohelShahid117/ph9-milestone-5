43b5cdd9cdf4cb6ae1d2e4dd2b4447848e1a022d
SohelShahid117 <sohelshahid09@gmail.com>
2024-02-15 23:43:11 +0600

    start from 15 min m-5,c-1

---
+++ conceptual-1/part-1-0minTo/js/script.js
@@ -0,0 +1,11 @@
+console.log(` conceptual-1 session running`);
+
+/*
+follow Rayans
+rayanser console e jao
+enter dao - window
+enter dao - document
+enter dao-console.dir(document)
+console.log(typeof document)
+dir is father of log
+*/
