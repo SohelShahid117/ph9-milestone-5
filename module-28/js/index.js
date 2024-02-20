17eae839fc7e3a4c393cf95b38e4fc9c3fa8ffa7
SohelShahid117 <sohelshahid09@gmail.com>
2024-02-20 09:15:48 +0600

    complete the code 28-2

---
+++ module-28/js/index.js
@@ -0,0 +1,90 @@
+// console.log(`connected`);
+// alert('connect');
+
+// 27-6 Change Home Screen to Playground by click
+/*
+function play() {
+    // console.log('play start now');
+
+    //step-1:hide the home screen-add hidden class
+    //step-2:show the playground-remove hidden
+
+    const homeScreen = document.getElementById('home-screen');
+    homeScreen.classList.add('hidden');
+
+    const playgroundScreen = document.getElementById('playground-screen');
+    playgroundScreen.classList.remove('hidden');
+}
+*/
+
+
+// 28-1 Project Review And Keyboard Event:
+// 28-2 Get The Key Pressed And Check Right Or Wrong Key Pressed
+function handleKeyboardKeyUpEvent(event){
+    // console.log('button press');
+    // console.log(event);
+    // console.log(event.key);
+    const playerPressed = event.key;
+    console.log('player press',playerPressed);
+
+    // expected press
+    const currentAlphabet = document.getElementById('current-alphabet').innerText.toLocaleLowerCase();
+    console.log('current alphabet',currentAlphabet)
+
+    //checked
+    if(currentAlphabet === playerPressed){
+        console.log('you win!You got a point.');
+    }else{
+        console.log('you loss! You missed a life');
+    }
+}
+document.addEventListener('keyup',handleKeyboardKeyUpEvent);
+
+// 27-7 Build getRandom alphabet Function.mp4:
+// 27-8 (Recap) Random Alphabet and highlight selected alphabet
+function continueGame() {
+    // step-1:generate a random alphabet
+    const alphabet = getARandomAlphabet();
+    console.log(alphabet);
+
+    // set-random alphabet to current-alphabet in playground screen
+
+    const currentAlphabet = document.getElementById('current-alphabet');
+    currentAlphabet.innerText = alphabet;
+    // document.getElementById('current-alphabet').innerText = alphabet;
+
+    setBackgroundColor(alphabet)
+
+}
+
+function play() {
+    hideElementById('home-screen');
+    showElementById('playground-screen');
+    continueGame();
+}
+
+// 27-9 Module Summary:all above
+
+
+
+
+// MDN syntax
+/*
+addEventListener("keyup", (event) => {});
+onkeyup = (event) => {};
+
+html:
+<input placeholder="Click here, then press and release a key." size="40" />
+<p id="log"></p>
+
+js:
+const input = document.querySelector("input");
+const log = document.getElementById("log");
+
+input.addEventListener("keyup", logKey);
+
+function logKey(e) {
+  log.textContent += ` ${e.code}`;
+}
+*/
+
---
+++ module-28/js/utility.js
@@ -0,0 +1,36 @@
+// 27-6 Change Home Screen to Playground by click
+function hideElementById(elementId) {
+    const element = document.getElementById(elementId);
+    element.classList.add('hidden');
+}
+function showElementById(elementId) {
+    const element = document.getElementById(elementId);
+    element.classList.remove('hidden');
+}
+
+// 27-8 (Recap) Random Alphabet and highlight selected alphabet
+function setBackgroundColor(elementId){
+    const element = document.getElementById(elementId);
+    element.classList.add('bg-amber-400');
+}
+
+// 27-7 Build getRandom alphabet Function.mp4:
+function getARandomAlphabet() {
+    // const alphabetStr = document.getElementsByClassName('kbd');
+    const alphabetStr = 'abcdefghijklmnopqrstuvwxyz'
+    const alphabetArr = alphabetStr.split('')
+    console.log(alphabetArr);
+
+    const randomNumberIndex = Math.round(Math.random() * 25);
+    console.log(randomNumberIndex);
+
+    const randomAlphabet = alphabetArr[randomNumberIndex];
+    console.log(randomNumberIndex, randomAlphabet);
+    return randomAlphabet;
+}
+
+// 27-9 Module Summary:all above
+
+
+
+
